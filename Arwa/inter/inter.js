e8f6cc7b4dc444faae9682a7f36d04a10271ae74
ArwaAlr <112874625+ArwaAlr@users.noreply.github.com>
2022-09-29 21:41:26 +0200

    Add files via upload

--- Arwa/inter/inter.js
+++ Arwa/inter/inter.js
@@ -3,12 +3,21 @@ let emp = JSON.parse(employee) ;
 console.table(emp);
 
 for (let emp1 of emp){
-    document.getElementById("contain").innerHTML += ("ID =" + " " + emp1.Unique_ID +  "  <br>"
-     + "The first name : "+emp1.First_Name + " <br>"
-     + "The last name : " + emp1.Last_Name + " <br>"
-     + "E-Mail adress : " + emp1.Email_address + " <br>" 
-     + "The job is : "  + ""+emp1.Job_Title + " " 
-     + " Salary : "   + emp1.Salary + " " 
-     + "<hr>") ;
+    document.getElementById("data").innerHTML += `<tr>
+        <th>${emp1.Unique_ID}</th>
+        <td>${emp1.First_Name}</td>
+        <td>${emp1.Last_Name}</td>
+        <td>${emp1.Email_address}</td>
+        <td>${emp1.Job_Title}</td>
+        <td>${emp1.Salary}</td>
+
+       </tr>`
+    //     "ID =" + " " + emp1.Unique_ID +  "  <br>"
+    //  + "The first name : "+emp1.First_Name + " <br>"
+    //  + "The last name : " + emp1.Last_Name + " <br>"
+    //  + "E-Mail adress : " + emp1.Email_address + " <br>" 
+    //  + "The job is : "  + ""+emp1.Job_Title + " " 
+    //  + " Salary : "   + emp1.Salary + " " 
+    //  + "<hr>"
     // console.log("The First Name : " + emp1.First_Name );
 }
